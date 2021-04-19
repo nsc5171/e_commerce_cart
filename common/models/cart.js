cb5122088dede79992f59f6985cfdc7de44f78a8
Sai Chandra N <nsaichandra55@gmail.com>
2021-04-20 03:53:03 +0530

    Added admin user to seed. Added postman collection and env files.

--- common/models/cart.js
+++ common/models/cart.js
@@ -7,6 +7,7 @@ module.exports = function Cart(Model) {
     Model.customValidations = utils.arrayify(Model.customValidations);
 
     Model.customValidations.push(function validateItemsPresence(data, options, next) {
-        if (utils.arrayify(data.items).length < 1) return utils.errorUtil.chainErrorOnCb(constants.ERROR_CODES.EMPTY_CART_ERR_01, {}, options, next);
+        if (utils.arrayify(data.items).length < 1) return utils.errorUtil.chainErrOnCb(constants.ERROR_CODES.EMPTY_CART_ERR_01, {}, options, next);
+        process.nextTick(next);
     });
 }
