df10d8f156c9130dc72b09022667831e0ea0b9e8
Sai Chandra N <nsaichandra55@gmail.com>
2021-04-24 02:23:03 +0530

    calculateCartPricing API

--- common/models/cart.js
+++ common/models/cart.js
@@ -17,6 +17,7 @@ module.exports = function cart(Cart) {
 
     Match.remoteMethod('calculateCartPricing', {
         accepts: [
+            { arg: 'data', required: true, type: "Cart", http: { source: "body" } },
             utils.options_arg_defn
         ],
         http: { verb: 'POST' },
@@ -24,6 +25,7 @@ module.exports = function cart(Cart) {
     });
 
     Cart.calculateCartPricing = function calculateCartPricing(data, options, next) {
+        data.items = utils.arrayify(data.items);
         let fnCtx = { itemTypes: Array.from(utils.arrayify(data && data.items).reduce((fin, curr) => { fin.add(curr.itemType); return fin; }, new Set())), itemPromotionCodes: new Set(), itemMasters: {} };
         async.waterfall([
             function fetchPromotionCodes(stepDone) {
@@ -48,7 +50,48 @@ module.exports = function cart(Cart) {
                     }
                 ], stepDone);
             },
-            
+            function fetchPromotionDefns(stepDone) {
+                async.parallel([
+                    function fetchCartPromotionRecs(threadDone) {
+                        utils.lb.findModel('CartPromotion').find({ where: { code: { inq: fnCtx.cartPromotionCodes } } }, options, (err, cartPromotionDefns) => {
+                            fnCtx.cartPromotionInsts = utils.arrayify(cartPromotionDefns).reduce((fin, curr) => {
+                                fin[curr.code] = curr;
+                                return fin;
+                            }, {});
+                            threadDone(err);
+                        })
+                    },
+                    function fetchItemPromotionRecs(threadDone) {
+                        utils.lb.findModel('ItemPromotion').find({ where: { code: { inq: Array.from(fnCtx.itemPromotionCodes) } } }, options, (err, itemPromotionDefns) => {
+                            fnCtx.itemPromotionInsts = utils.arrayify(itemPromotionDefns).reduce((fin, curr) => {
+                                fin[curr.code] = curr;
+                                return fin;
+                            }, {});
+                            threadDone(err);
+                        })
+                    }
+                ], stepDone);
+            },
+            function processItemPromotions(stepDone) {
+                data.items.forEach(item => {
+                    let itemMaster = fnCtx.itemMasters[item.itemType];
+                    item.perItemMRP = itemMaster.MRP;
+                    utils.arrayify(itemMaster.promotionCodes).forEach(promotionCode => {
+                        fnCtx.itemPromotionInsts[promotionCode].applyPromotion(item);
+                    })
+                });
+                process.nextTick(stepDone);
+            },
+            function processCartPromotions(stepDone) {
+                data.items.forEach(item => {
+                    data.itemsDiscount += item.discount;
+                    data.MRP += (item.perItemMRP * item.quantity);
+                })
+                utils.arrayify(fnCtx.cartPromotionCodes).forEach(promotionCode => {
+                    fnCtx.cartPromotionInsts[promotionCode].applyPromotion(data);
+                })
+                process.nextTick(stepDone);
+            }
         ], err => next(err, data));
     }
 }
